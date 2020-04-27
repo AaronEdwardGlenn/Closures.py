c305cb09e773b4ce0d029ccd4dc028ea579b4e32
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-26 21:02:52 -0700

    created closure for add and subtract fuctions that loggs each function into a separage file

--- closures_ex2.py
+++ closures_ex2.py
@@ -4,7 +4,7 @@ logging.basicConfig(filename='example.log', level=logging.INFO)
 
 def logger(func):
     def log_func(*args):  # the star here means we can add in any number of arguments into the function
-        logging.info('Running "{}" with arguments {}').format(func.__name__, args)
+        logging.info('Running "{}" with arguments {}'.format(func.__name__, args))
         print(func(*args))
     return log_func
 
@@ -18,3 +18,10 @@ def sub(x, y):
 
 
 add_logger = logger(add)
+sub_logger = logger(sub)
+
+add_logger(3, 3)
+add_logger(4, 5)
+
+sub_logger(10, 5)
+sub_logger(20, 11)
